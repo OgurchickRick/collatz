db1432bec0ff62632125c0b667fc553d5ba538fe
OgurchickRick <98107660+OgurchickRick@users.noreply.github.com>
2022-01-26 17:51:04 +0300

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,30 +1,13 @@
-x = int(input('Введите положительное число:'))
-A = []
-
-if x > 0:
-    def x2(x):
-        x = x / 2
-        return collatz(x)
+def X_input ()
+#Тихон
 
 
-    def x3_1(x):
-        x = x * 3 + 1
-        return collatz(x)
+def x2 ()
+#Вика
 
 
-    def collatz(x):
-        if x == 1:
-            A.append(int(x))
-            return  x
-        elif x % 2 == 0:
-            A.append(int(x))
-            return x2(x)
-        else:
-            A.append(int(x))
-            return x3_1(x)
+def x3_1 ()
+#Денис
 
 
-    collatz(x)
-    print(A)
-else:
-    print("Введены некорректные данные")
+def collatz (x)
