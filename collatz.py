f3fa9d0f524e0ffb7f67104af07af866ff0253a1
OgurchickRick <98107660+OgurchickRick@users.noreply.github.com>
2022-01-26 21:19:39 +0300

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,18 +1,31 @@
-def X_input ():
-  while True:
-    n = int(input('Введите натуральное число:'))
-    if n > 0:
-      break
-    print('Неверные данные')
-  return n
+def x_input ():
+    while True:
+        n = int(input('Введите натуральное число:'))
+        if n > 0:
+            break
+        print('Неверные данные')
+    return n
 
 
 def x2 (n):
-  return collatz(n//2)
+    return collatz(n//2)
 
 
-def x3_1 (n)
-  return collatz(n*3+1)
+def x3_1 (n):
+    return collatz(n*3+1)
 
 
-def collatz (x)
+def collatz (n):
+    result = [n]
+    if n == 1:
+        pass
+    elif n % 2 == 0:
+        result.extend(x2())
+    else:
+        result.extend(x3_1())
+    return result
+
+
+if __name__ == "__main__":
+    x = x_input ()
+    print (collatz(x))
